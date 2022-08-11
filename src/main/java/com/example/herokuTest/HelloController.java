875009d6fc6c6ed7a540d8a68c4a6bf4d15d9b96
Yannik <yannik.wegner@stud.hs-ruhrwest.de>
2022-08-11 14:39:53 +0200

    initial commit

---
+++ src/main/java/com/example/herokuTest/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.herokuTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    String hello() {
+        return "Hello World";
+    }
+}
